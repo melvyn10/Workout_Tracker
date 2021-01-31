b8c405ef42c5e9132d0dcb5608d172a7c8134da7
melvyn10 <melvyn.ten@ge.com>
2021-01-31 13:00:42 -0500

    deployuing

--- server.js
+++ server.js
@@ -16,7 +16,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout',
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+
+}
+);
+    
 
 // Route to post form submission to mongoDB via mongoose
 app.post("/api/workouts", ({body}, res) => {
